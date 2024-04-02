c5df1aa10c9fb68ebce1b2e1b858c868318cce21
nickgrigojev <[ns.grigorjev@gmail.com])>
2024-04-02 09:59:03 +0400

    Initial commit

---
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,10 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+public class Main {
+    public static void main(String[] args) {
+        // реализуйте алгоритм здесь
+        UserServiceImpl userService = new UserServiceImpl();
+    }
+}
